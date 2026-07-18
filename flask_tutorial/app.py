cf8b0c2febd7f3a4f6b501092ea455ade4425bb4
Ben S <3.1415ben@gmail.com>
2026-07-18 17:32:29 -0500

    Added flask compadability with backend

--- flask_tutorial/app.py
+++ flask_tutorial/app.py
@@ -1,10 +1,49 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
+from quiz_bowl_flask import Game
 
 app = Flask(__name__)
 
+game = Game()
+
+game.cat = ["Science"]
+game.diff = ["1"]
+strict = 0.8
+
 @app.route("/")
 def home():
-    return "Leave my sweater on the porch"
+    if game.current_question is None:
+        game.next_question()
+    return render_template(
+        "index.html",
+        question=game.current_question.text,
+        answer=game.current_question.answer
+    )
+
+@app.route("/answer", methods=["POST"])
+def answer():
+
+    question = game.current_question.text
+    user_answer = request.form["user_answer"]
+    
+    if game.submit_answer(user_answer):
+        result = "Correct!"
+    else:
+        result = "Incorrect!"
+    
+    
+
+    return render_template(
+        "index.html",
+        question=question,
+        result=result,
+        score = game.score
+    )
+
+@app.route("/next", methods=["POST"])
+def next():
+    game.next_question()
+
+    return redirect(url_for("home"))
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ flask_tutorial/quiz_bowl_flask.py
@@ -0,0 +1,115 @@
+import requests
+import json
+import jellyfish
+import re
+tossup_url = "https://www.qbreader.org/api/random-tossup?"
+
+
+
+class Game:
+    def __init__(
+        self,
+        score=0,
+        user=None,
+        powers=None,
+        not_powers=None,
+        current_question=None,
+        cat=None,
+        diff=None,
+        speed=None,
+        user_answer=None,
+        strict=0
+    ):
+        self.score = score
+        self.user = user
+        self.powers = powers
+        self.not_powers = not_powers
+        self.user_answer = user_answer
+        self.current_question = current_question
+        self.cat = cat if cat else []
+        self.diff = diff if diff else []
+        self.speed = speed
+        self.strict = strict
+
+    def next_question(self):
+        self.current_question = get_data(1, self.cat, self.diff)
+  
+    def submit_answer(self, user_answer):
+        correct = self.current_question.check(user_answer, self.strict)
+
+        if correct:
+            self.score += 10
+        else:
+            self.score -= 5
+
+        return correct
+
+class Question:
+    def __init__(self, text, answer, cat, diff):
+        self.text = text
+        self.answer = answer
+        self.cat = cat
+        self.diff = diff
+
+        self.position = 0
+        self.done = False
+   
+    def check(self, user_answer, strict):
+        user_answer = user_answer.strip().lower()
+        answers = re.findall(r"<u>(.*?)</u>", self.answer)
+
+        if not answers:
+            answers = [self.answer]
+
+        for answer in answers:
+            answer = answer.lower().strip()
+
+            similarity = jellyfish.jaro_winkler_similarity(
+                answer, user_answer
+            )
+            
+            if similarity >= strict:
+                return True
+  
+        return False
+    
+def get_data(number, cat, diff):
+
+    params = {
+        "number": number,
+        "categories": cat,
+        "difficulties": diff,
+    }
+
+    response = requests.get(tossup_url, params=params)
+    data = response.json()
+
+    tossup = data["tossups"][0]
+
+    return Question(
+        tossup["question"],
+        tossup["answer"],
+        tossup["category"],
+        tossup["difficulty"]
+    )
+
+
+def test():
+
+    game = Game()
+
+    while True:
+        game.current_question = get_data(1, ["Science"], ["1"])
+
+        print(game.current_question.text)
+
+        user_answer = input("Answer: ")
+
+        if game.current_question.check(user_answer, 0.8):
+            print("Correct")
+            game.score += 10
+        else:
+            print("Incorrect")
+            game.score -= 5
+
+        print(game.score)
